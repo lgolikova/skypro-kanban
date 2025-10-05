432c81ba0ac88f19f82929fcb4ac7f99d4d5ba16
lgolikova <milagoul@gmail.com>
2025-10-05 17:35:56 +0300

    made popup window from PopUser component

--- src/components/popups/PopUser/PopUser.jsx
+++ src/components/popups/PopUser/PopUser.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-function PopUser() {
+function PopUser({ isOpen }) {
     return (
-        <div className="header__pop-user-set pop-user-set" id="user-set-target">
+        <div className="header__pop-user-set pop-user-set" id="user-set-target" style={{ display: isOpen ? "block" : "none" }}>
         <p className="pop-user-set__name">Ivan Ivanov</p>
         <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
         <div className="pop-user-set__theme">
