95f9535a22176a1f6963fc5d006c03670a648539
lgolikova <milagoul@gmail.com>
2025-10-05 20:43:15 +0300

    added loader

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,41 @@
+import React, { useState, useEffect } from "react";
 import Column from '../Column/Column'
 import {cardList} from "../../data"
 
 function Main() {
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (isLoading) {
+        return (
+            <main className="main">
+                <div className="container">
+                    <div
+                        style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "60vh",
+                        fontSize: "20px",
+                        color: "#94A6BE",
+                        fontWeight: "500",
+                        }}
+                    >
+                        Данные загружаются...
+                    </div>
+                </div>
+            </main>
+        );
+    }
+
     const statuses = [
         "Без статуса",
         "Нужно сделать",
@@ -12,20 +46,21 @@ function Main() {
 
     return (
         <main className="main">
-        <div className="container">
-            <div className="main__block">
+            <div className="container">
+                <div className="main__block">
                 <div className="main__content">
-                    {statuses.map((status) => {
-                        const cardsByStatus = cardList.filter(
-                        (card) => card.status === status
-                    );
-                    return <Column key={status} title={status} cards={cardsByStatus} />;
-                })}
+                    {statuses.map((status) => (
+                    <Column
+                        key={status}
+                        title={status}
+                        cards={cardList.filter((card) => card.status === status)}
+                    />
+                    ))}
+                </div>
                 </div>
             </div>
-        </div>
         </main>
-    )
+    );
 }
 
 export default Main
