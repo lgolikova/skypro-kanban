d7976ac4655f2a512ddcb4f9769b7887cbe80c1a
lgolikova <milagoul@gmail.com>
2025-10-08 19:01:38 +0300

    card styled component almost done

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,32 +1,33 @@
 import React from "react";
+import { SCardsItem, SCard, SCardGroup, SCardContent, SCardTitle, SCardDate, SCardBtn } from './Card.styled'
 
 function Card({ topic, title, date }) {
     return (
-        <div className="cards__item">
-            <div className="cards__card card">
-            <div className="card__group">
-                <div className={`card__theme _${topic.toLowerCase().replace(" ", "-")}`}>
-                <p>{topic}</p>
-                </div>
-                <a href="#popBrowse" target="_self">
-                <div className="card__btn">
-                    <div></div><div></div><div></div>
-                </div>
-                </a>
-            </div>
-            <div className="card__content">
-                <a href="#" target="_blank">
-                <h3 className="card__title">{title}</h3>
-                </a>
-                <div className="card__date">
-                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
-                    <path d="M10.5625 2.03125H2.4375C2.00304 2.03125 1.65625 2.37804 1.65625 2.8125V10.1875C1.65625 10.622 2.00304 10.9688 2.4375 10.9688H10.5625C10.997 10.9688 11.3438 10.622 11.3438 10.1875V2.8125C11.3438 2.37804 10.997 2.03125 10.5625 2.03125ZM10.9688 10.1875C10.9688 10.3976 10.7726 10.5938 10.5625 10.5938H2.4375C2.22739 10.5938 2.03125 10.3976 2.03125 10.1875V4.15625H10.9688V10.1875ZM10.9688 3.78125H2.03125V2.8125C2.03125 2.60239 2.22739 2.40625 2.4375 2.40625H10.5625C10.7726 2.40625 10.9688 2.60239 10.9688 2.8125V3.78125Z" stroke="#94A6BE" strokeWidth="0.8" />
-                </svg>
-                <p>{date}</p>
-                </div>
-            </div>
-            </div>
-        </div>
+        <SCardsItem>
+            <SCard>
+                <SCardGroup>
+                    <div className={`card__theme _${topic.toLowerCase().replace(" ", "-")}`}>
+                        <p>{topic}</p>
+                    </div>
+                    <a href="#popBrowse" target="_self">
+                    <SCardBtn>
+                        <div></div><div></div><div></div>
+                    </SCardBtn>
+                    </a>
+                </SCardGroup>
+                <SCardContent>
+                    <a href="#" target="_blank">
+                    <SCardTitle>{title}</SCardTitle>
+                    </a>
+                    <SCardDate>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
+                            <path d="M10.5625 2.03125H2.4375C2.00304 2.03125 1.65625 2.37804 1.65625 2.8125V10.1875C1.65625 10.622 2.00304 10.9688 2.4375 10.9688H10.5625C10.997 10.9688 11.3438 10.622 11.3438 10.1875V2.8125C11.3438 2.37804 10.997 2.03125 10.5625 2.03125ZM10.9688 10.1875C10.9688 10.3976 10.7726 10.5938 10.5625 10.5938H2.4375C2.22739 10.5938 2.03125 10.3976 2.03125 10.1875V4.15625H10.9688V10.1875ZM10.9688 3.78125H2.03125V2.8125C2.03125 2.60239 2.22739 2.40625 2.4375 2.40625H10.5625C10.7726 2.40625 10.9688 2.60239 10.9688 2.8125V3.78125Z" stroke="#94A6BE" strokeWidth="0.8" />
+                        </svg>
+                        <p>{date}</p>
+                    </SCardDate>
+                </SCardContent>
+            </SCard>
+        </SCardsItem>
         )
     }
 
---
+++ src/components/Card/Card.styled.js
@@ -0,0 +1,91 @@
+import styled, { keyframes } from "styled-components";
+
+const cardAnimation = keyframes`
+    0% {
+    max-height: 0;
+    opacity: 0;
+    transform: scale(0.95);
+    }
+    100% {
+    max-height: 500px;
+    opacity: 1;
+    transform: scale(1);
+    }
+`;
+
+export const SCardsItem = styled.div`
+    padding: 5px;
+    overflow: hidden;
+    animation: ${cardAnimation} 0.5s ease-out;
+`;
+
+export const SCard = styled.div`
+    width: 220px;
+    height: 130px;
+    background-color: #ffffff;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: stretch;
+    padding: 15px 13px 19px;
+`;
+
+export const SCardGroup = styled.div`
+    width: 100%;
+    height: 20px;
+    margin-bottom: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const SCardContent = styled.div`
+    height: 64px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+`;
+
+export const SCardTitle = styled.h3`
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 18px;
+    color: #000000;
+    margin-bottom: 10px;
+`;
+
+export const SCardDate = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    svg {
+        width: 13px;
+    }
+
+    p {
+        margin-left: 6px;
+        font-size: 10px;
+        line-height: 13px;
+        color: #94a6be;
+        letter-spacing: 0.2px;
+    }
+`;
+
+export const SCardBtn = styled.div`
+    width: 24px;
+    height: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    padding: 2px;
+
+    div {
+        width: 4px;
+        height: 4px;
+        border-radius: 50%;
+        background-color: #94a6be;
+    }
+`;
