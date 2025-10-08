fa6e96e70630d74eb05e3061a0a49456afb5d66c
lgolikova <milagoul@gmail.com>
2025-10-08 21:09:34 +0300

    PopExit styled component in progress

--- src/components/popups/PopExit/PopExit.jsx
+++ src/components/popups/PopExit/PopExit.jsx
@@ -1,26 +1,27 @@
 import React from "react";
+import {SPopExit, SPopExitContainer, SPopExitBlock, SPopExitTtl} from './PopExit.styled';
 
 function PopExit() {
     return (
-        <div className="pop-exit" id="popExit">
-            <div className="pop-exit__container">
-            <div className="pop-exit__block">
-                <div className="pop-exit__ttl">
-                <h2>Выйти из аккаунта?</h2>
-                </div>
-                <form className="pop-exit__form" id="formExit" action="#">
-                <div className="pop-exit__form-group">
-                    <button className="pop-exit__exit-yes _hover01" id="exitYes">
-                    <a href="modal/signin.html">Да, выйти</a>
-                    </button>
-                    <button className="pop-exit__exit-no _hover03" id="exitNo">
-                    <a href="main.html">Нет, остаться</a>
-                    </button>
-                </div>
-                </form>
-            </div>
-            </div>
-        </div>
+        <SPopExit id="popExit">
+            <SPopExitContainer>
+                <SPopExitBlock>
+                    <SPopExitTtl>
+                        <h2>Выйти из аккаунта?</h2>
+                    </SPopExitTtl>
+                    <form className="pop-exit__form" id="formExit" action="#">
+                        <div className="pop-exit__form-group">
+                            <button className="pop-exit__exit-yes _hover01" id="exitYes">
+                                <a href="modal/signin.html">Да, выйти</a>
+                            </button>
+                            <button className="pop-exit__exit-no _hover03" id="exitNo">
+                                <a href="main.html">Нет, остаться</a>
+                            </button>
+                        </div>
+                    </form>
+                </SPopExitBlock>
+            </SPopExitContainer>
+        </SPopExit>
     );
 }
 
