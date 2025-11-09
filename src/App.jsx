1f65db4c07a5c0719ef3998308e48a765631445f
lgolikova <milagoul@gmail.com>
2025-11-09 11:52:16 +0300

    added AuthContext

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import './App.css';
 import GlobalStyles from "./components/GlobalStyles";
 import { SWrapper } from './components/Wrapper.styled';
 import AppRoutes from './AppRoutes';
+import AuthProvider from "./context/AuthProvider";
 
 function App() {
   return (
     <>
+      <AuthProvider>
         <SWrapper>
             <GlobalStyles />
             <AppRoutes />
         </SWrapper>
+      </AuthProvider>
     </>
   )
 }
--- src/components/popups/PopUser/PopUser.jsx
+++ src/components/popups/PopUser/PopUser.jsx
@@ -1,22 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { SPopUserSet, SPopUserSetName, SPopUserSetMail, SPopUserSetTheme } from "./PopUser.styled"
+import React, { useEffect, useState, useContext } from "react";
+import { SPopUserSet, SPopUserSetName, SPopUserSetMail, SPopUserSetTheme } from "./PopUser.styled";
+import { AuthContext } from "../../../context/AuthContext";
 
 function PopUser({ isOpen, onClose, onRequestExit }) {
-    const [userName, setUserName] = useState("");
-    const [userEmail, setUserEmail] = useState("");
+    // const [userName, setUserName] = useState("");
+    // const [userEmail, setUserEmail] = useState("");
+    const { user } = useContext(AuthContext);
 
-    useEffect(() => {
-        if (isOpen) {
-            const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-            if (userInfo) {
-                setUserName(userInfo.user?.name || userInfo.name || "Пользователь");
-                setUserEmail(userInfo.user?.login || userInfo.login || "—");
-            }
-        }
-    }, [isOpen]);
+    // useEffect(() => {
+    //     if (isOpen) {
+    //         const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    //         if (userInfo) {
+    //             setUserName(userInfo.user?.name || userInfo.name || "Пользователь");
+    //             setUserEmail(userInfo.user?.login || userInfo.login || "—");
+    //         }
+    //     }
+    // }, [isOpen]);
 
 
     if (!isOpen) return null;
+
+    const userName = user?.user?.name || user?.name || "Пользователь";
+    const userEmail = user?.user?.login || user?.login || "—";
 
     return (
         <SPopUserSet>
